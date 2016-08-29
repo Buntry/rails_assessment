7e44604c41ea851736dd62c3834f7a3407804717
Buntry <parkergriep@yahoo.com>
2016-08-29 11:36:47 -0400

    rails template files. omniauth initializer.

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+	root 'recipes#index'
+	
+	resources :users
+	delete '/users/:id' => 'users#destroy', as: 'destroy_user'
+	
+	#Sessions
+	get  '/login'  => 'sessions#new'
+	post '/login'  => 'sessions#create'
+	get  '/logout' => 'sessions#destroy'
+	
+	#Omniauth
+	get "/auth/:provider/callback" => "sessions#create_with_omniauth"
+	get '/omniauth-login' => 'sessions#sign_in_with_omniauth', as: 'o_login'
+	
+	resources :recipes
+	delete '/recipes/:id' => 'recipes#destroy', as: 'destroy_recipe'
+	
+	get '/ingredients' => 'ingredients#index', as: 'ingredients'
+end
